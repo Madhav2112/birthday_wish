51588ee73fbcc62449d4b62919797595b01a2a0a
Madhav <madhav.taneja01@gmail.com>
2026-01-11 19:58:04 +0400

    restore-working: verified Sheets locally

---
+++ print-env.js
@@ -0,0 +1,6 @@
+﻿require('dotenv').config({ path: '.env.local' });
+console.log('ENV FILE LOADED: .env.local');
+console.log('GOOGLE_PRIVATE_KEY_B64 length:', (process.env.GOOGLE_PRIVATE_KEY_B64 || '').length);
+console.log('HAS GOOGLE_PRIVATE_KEY_B64:', !!process.env.GOOGLE_PRIVATE_KEY_B64);
+console.log('GOOGLE_CLIENT_EMAIL:', process.env.GOOGLE_CLIENT_EMAIL || '<missing>');
+console.log('GOOGLE_SHEET_ID:', process.env.GOOGLE_SHEET_ID || '<missing>');
